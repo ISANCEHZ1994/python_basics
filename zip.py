2628988fb8cfc2bcfdc9edd0930b97bded406564
Israel Sanchez <Israel.pro1994@gmail.com>
2023-02-03 18:32:14 -0600

    notes on zip function

--- zip.py
+++ zip.py
@@ -3,6 +3,7 @@
 my_list = [ 1, 2 ,3 ];
 your_list = [ 10, 20, 30 ];
 your_list2 = ( 10, 20, 30 );
+their_list = [ 5, 6, 7 ]
 
 def multiply_by2(item):
     return item * 2
@@ -10,5 +11,7 @@ def multiply_by2(item):
 def only_odd(item):
     return item % 2 != 0
 
-print(list(zip(my_list, your_list)))  # [(1, 10), (2, 20), (3, 30)]
-print(list(zip(my_list, your_list2))) # should look the same as above! even with paraenthesis 
+# it all depends on ORDER
+print(list(zip(their_list, my_list, your_list)))  # [(1, 10), (2, 20), (3, 30)] => now changed to [(5, 1, 10), (6, 2, 20), (7, 3, 30)]
+print(list(zip(my_list, your_list2, their_list))) # should look the same as above! even with paraenthesis 
+# might not look important now - think about - two collections of data and we need to combine them into a temple
